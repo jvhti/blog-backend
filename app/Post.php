aeea1ab05069fd85103de19ec3b9ad54caf5c7d0
João Víctor <jvhti@hotmail.com>
2019-02-04 19:00:06 -0200

    Add Some Relationships to 'Comment, Post & User' Models

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    public function author()
+    {
+        return $this->belongsTo('App\User', 'createdBy_user_id');
+    }
+
+    public function lastEditor()
+    {
+        return $this->belongsTo('App\User', 'updatedBy_user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tags');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function reports()
+    {
+        return $this->hasMany('App\Report');
+    }
+
 }
