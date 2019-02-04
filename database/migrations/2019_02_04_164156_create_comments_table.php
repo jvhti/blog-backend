8e2da184110cd6913db03cc850227ff1b68f2b38
João Víctor <jvhti@hotmail.com>
2019-02-04 14:47:32 -0200

    Create 'Comments' Table

---
+++ database/migrations/2019_02_04_164156_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('comment');
+            $table->unsignedInteger('createdBy')->nullable();
+            $table->unsignedInteger('editedBy')->nullable();
+            $table->boolean('visible');
+            $table->unsignedInteger('parentComment')->nullable();
+            $table->ipAddress('ipAddress');
+            $table->boolean('removed');
+            $table->string('removedCause')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
