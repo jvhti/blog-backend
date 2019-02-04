9b2b2a4f5377971afc0a595a87827bcb95536d5c
João Víctor <jvhti@hotmail.com>
2019-02-04 16:09:52 -0200

    Change Seeders Order (Seed Tags before Posts)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
           UsersTableSeeder::class,
-          PostsTableSeeder::class,
           TagsTableSeeder::class,
+          PostsTableSeeder::class,
           CommentsTableSeeder::class,
           ReportsTableSeeder::class
         ]);
