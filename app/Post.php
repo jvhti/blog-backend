cf3a8366b583aa655e5741820b3ce1a3004708d7
João Víctor <jvhti@hotmail.com>
2019-02-05 01:17:03 -0200

    Update 'Post & Tag' Models

--- app/Post.php
+++ app/Post.php
@@ -19,7 +19,7 @@ class Post extends Model
 
     public function tags()
     {
-        return $this->belongsToMany('App\Tag');
+        return $this->belongsToMany('App\Tag', 'post_has_tag');
     }
 
     public function comments()
